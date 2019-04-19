61f96310fd9d58e86ba0351fc0a2b6b94dee5a4f
juanma2424 <42756436+juanma2424@users.noreply.github.com>
2019-04-19 04:22:43 -0600

    Prueba

---
+++ match.ts
@@ -0,0 +1,154 @@
+export class mats {
+
+    private zoneA: number[][];
+    private zoneB: number[][];
+    private zoneC: number[][];
+
+    private zoneD: number[][];
+    private zoneE: number[][];
+    private zoneF: number[][];
+
+    private zoneG: number[][];
+    private zoneH: number[][];
+
+    private audioData: any;
+
+    public constructor(pAudioData: any) {
+        this.zoneA = [];
+        this.zoneB = [];
+        this.zoneC = [];
+        this.zoneD = [];
+        this.zoneE = [];
+        this.zoneF = [];
+        this.zoneG = [];
+        this.zoneH = [];
+        this.audioData = pAudioData;
+    }
+
+    public splitSong() {
+        var pSpliteSize: number = 1;// cantidad de pisos
+        var audioLength = this.audioData.channelData[0].length;// largo del audio
+        var accepted: number;//
+
+        var firstTime: boolean = true;
+        var lastZone: number = 0;
+        var nowZone: number = 0;
+
+        var point: number;//temp para guardar los datos de una zona 
+        var zone: number[] = [];//temp para guardar los datos de una zona 
+
+        switch (pSpliteSize) {
+
+            case 1: {
+                for (var i = 0; i < audioLength; i++) {
+                    accepted = Math.floor(Math.random() * 2);//switch de canales
+
+                    if (accepted < 2) {
+
+                        point = this.audioData.channelData[0][i];
+
+                        if (!firstTime) {// sino es la primera vez
+
+                            // asigna nuevo valor a nowZone
+                            if (point >= 0.5) { nowZone = 1 }
+                            else if (point >= 0) { nowZone = 2 }
+                            else if (point >= -0.5) { nowZone = 3 }
+                            else { nowZone = 4 }
+
+                            if (lastZone == nowZone) {// si las zonas son iguales sigue anadiendo puntos
+                                zone.push(point);
+                            } 
+                            else {//si las zonas cambian o sea se brinca de una zona a otra 
+                                this.insertZone(lastZone, Object.assign([], zone));// guarda los datos de toda la zona pasada
+                                zone = [];// refresca el temp de lazona
+                                lastZone = nowZone;
+                                zone.push(point);
+                            }
+                        } else {// solo para el primer caso
+                            zone.push(point);
+                            if (point >= 0.5) { lastZone = nowZone = 1 }///ZONE A
+                            else if (point >= 0) { lastZone = nowZone = 2 }//ZONE B
+                            else if (point >= -0.5) { lastZone = nowZone = 3 }// ZONE C
+                            else { lastZone = nowZone = 4 }// ZONE D
+                            firstTime = false;
+                        }
+                    }
+                }
+
+            }
+
+
+            case 2: {
+                for (var i = 0; i < audioLength; i++) {
+                    accepted = Math.floor(Math.random() * 2);//switch de canales
+
+                    if (accepted < 2) {
+
+                        point = this.audioData.channelData[0][i];
+
+                        if (!firstTime) {// sino es la primera vez
+
+                            // asigna nuevo valor a nowZone
+                            if (point >= 0.6) { nowZone = 1}
+                            else if (point >= 0.3) { nowZone = 2 }
+                            else if (point >= 0) { nowZone = 3 }
+                            else if (point >= -0.3) { nowZone = 4 }
+                            else if (point >= -0.6) { nowZone = 5 }
+                            else { nowZone = 6 }
+
+                            if (lastZone == nowZone) {// si las zonas son iguales sigue anadiendo puntos
+                                zone.push(point);
+                            } 
+                            else {//si las zonas cambian o sea se brinca de una zona a otra 
+                                this.insertZone(lastZone, Object.assign([], zone));// guarda los datos de toda la zona pasada
+                                zone = [];// refresca el temp de lazona
+                                lastZone = nowZone;
+                                zone.push(point);
+                            }
+                        } else {// solo para el primer caso
+                            zone.push(point);
+                            if (point >= 0.6) {lastZone = nowZone = 1}
+                            else if (point >= 0.3) { lastZone = nowZone = 2 }
+                            else if (point >= 0) { lastZone = nowZone = 3 }
+                            else if (point >= -0.3) { lastZone = nowZone = 4 }
+                            else if (point >= -0.6) { lastZone = nowZone = 5 }
+                            else {lastZone = nowZone = 6}
+                            firstTime = false;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    private insertZone(pZoneNumber: number, pZone: number[]) {
+        switch (pZoneNumber) {
+            case 1: {
+                this.zoneA.push(pZone);
+                break;
+            }
+            case 2: {
+                this.zoneB.push(pZone);
+                break;
+            }
+            case 3: {
+                this.zoneC.push(pZone);
+                break;
+            }
+            case 4:{
+                this.zoneD.push(pZone);
+                break;
+            }
+            case 5:{
+                this.zoneE.push(pZone);
+                break;
+            }
+            case 6:{
+                this.zoneF.push(pZone);
+                break;
+            }
+            
+        }
+    }
+}
---
+++ sampleS2.ts
@@ -0,0 +1,60 @@
+export class s2 {
+
+    public zonesStr: string[];
+    public zones: number[];
+
+    private BeginZone: string;
+    private FinalZone: string;
+
+    private BeginPoint: number;
+    private FinalPoint: number;
+
+    private audioData: any;
+
+    public constructor(pAudioData: any) {
+        this.zones = [];
+        this.zonesStr = [];
+        this.BeginPoint = 0;
+        this.FinalPoint = 0;
+        this.BeginZone = "";
+        this.FinalZone = "";
+        this.audioData = pAudioData;
+    }
+
+    public mainComponent() {
+
+        var audioLength = this.audioData.channelData[0].length;// largo del audio
+        var nowZone: string = "";
+
+        var point: number;//temp para guardar los datos de una zona 
+        var zone: number[] = [];//temp para guardar los datos de una zona 
+        var zonesStr: string[] = [];//temp para guardar los datos de una zona 
+
+        this.BeginPoint = this.audioData.channelData[0][0];
+        this.FinalPoint = this.audioData.channelData[0][audioLength-1];
+        this.zones.push(this.BeginPoint);
+        this.zones.push(this.FinalPoint);
+
+       
+        for (var i = 0; i <= this.zones.length-1 ; i++) {
+            point = this.zones[i];
+            console.log('///////////////////////'+ point);
+            if (point >= 0.5) { nowZone = "A" }
+            else if (point >= 0) { nowZone = "B" }
+            else if (point >= -0.5) { nowZone = "C" }
+            else { nowZone = "D" }
+            this.zonesStr.push(nowZone);
+        }
+
+
+        // console.log('*******************');
+        // console.log(zone[0]);
+        // console.log(zone[1]);
+        // console.log('*******************');
+        // console.log(zonesStr[0]);
+        // console.log(zonesStr[1]);
+    }
+
+
+
+}

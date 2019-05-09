f3aa4aa6dc5154a07facda5d22bdd0b798beee8d
juanma2424 <42756436+juanma2424@users.noreply.github.com>
2019-05-09 13:51:17 -0600

    fail area 2 back area 1

--- dist/area.js
+++ dist/area.js
@@ -5,7 +5,7 @@ var areas = /** @class */ (function () {
     }
     areas.prototype.waveTiangle = function (pAX, pAY, pBX, pBY, pCX, pCY) {
         var areaTriangle = Math.abs((((pAX * (pBY - pCY)) + (pBX * (pCY - pAY)) + (pCX * (pAY - pBY))) / 2));
-        return areaTriangle * 100;
+        return (areaTriangle * 100) * 2;
     };
     return areas;
 }());
